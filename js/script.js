dbac7cab433780d6cf13cde75faaa3a2de5b8e65
Harsh <harshj7.net@gmail.com>
2023-01-14 21:37:41 +0530

    work on every condition in js and working copy btn

--- js/script.js
+++ js/script.js
@@ -16,17 +16,33 @@ function metadata() {
             refresh = 3000000;
             revisit = 5;
             document.meta_form.result.value = `<meta name="title" content="${name}">` + "\n" + `<meta name="description" content="${disc}">` + "\n" + `<meta name="keywords" content="${key}">` + "\n" + `<meta name="author" content="${auth}">` + "\n" + `<meta property="og:url" content="${url}">` + "\n" + `<meta property="og:title" content="${name}">` + "\n" + `<meta property="og:description" content="${disc}">` + "\n" + `<meta property="og:image" itemprop="image" content="${img}">` + "\n" + `<meta http-equiv="refresh" content="${refresh}">` + "\n" + `<meta name="revisit-after" content="${revisit}days">` + "\n" + `<meta property="og:type" content="website">` + "\n" + `<meta name="robots" content="index,follow">`;
+            document.meta_form.copybtn.value = "Copy 📋";
+            document.getElementById("copy-btn").style.backgroundColor = "rgb(1, 64, 254)";
+            document.getElementById("resultinp").style.display = "flex";
+            document.getElementById("copy-btn").style.display = "flex";
         }
         else if (meta_form.meta_refreshsec.value == "") {
             refresh = 3000000;
             document.meta_form.result.value = `<meta name="title" content="${name}">` + "\n" + `<meta name="description" content="${disc}">` + "\n" + `<meta name="keywords" content="${key}">` + "\n" + `<meta name="author" content="${auth}">` + "\n" + `<meta property="og:url" content="${url}">` + "\n" + `<meta property="og:title" content="${name}">` + "\n" + `<meta property="og:description" content="${disc}">` + "\n" + `<meta property="og:image" itemprop="image" content="${img}">` + "\n" + `<meta http-equiv="refresh" content="${refresh}">` + "\n" + `<meta name="revisit-after" content="${revisit}days">` + "\n" + `<meta property="og:type" content="website">` + "\n" + `<meta name="robots" content="index,follow">`;
+            document.meta_form.copybtn.value = "Copy 📋";
+            document.getElementById("copy-btn").style.backgroundColor = "rgb(1, 64, 254)";
+            document.getElementById("resultinp").style.display = "flex";
+            document.getElementById("copy-btn").style.display = "flex";
         }
         else if (meta_form.meta_enginevisit.value == "") {
             revisit = 5;
             document.meta_form.result.value = `<meta name="title" content="${name}">` + "\n" + `<meta name="description" content="${disc}">` + "\n" + `<meta name="keywords" content="${key}">` + "\n" + `<meta name="author" content="${auth}">` + "\n" + `<meta property="og:url" content="${url}">` + "\n" + `<meta property="og:title" content="${name}">` + "\n" + `<meta property="og:description" content="${disc}">` + "\n" + `<meta property="og:image" itemprop="image" content="${img}">` + "\n" + `<meta http-equiv="refresh" content="${refresh}">` + "\n" + `<meta name="revisit-after" content="${revisit}days">` + "\n" + `<meta property="og:type" content="website">` + "\n" + `<meta name="robots" content="index,follow">`;
+            document.meta_form.copybtn.value = "Copy 📋";
+            document.getElementById("copy-btn").style.backgroundColor = "rgb(1, 64, 254)";
+            document.getElementById("resultinp").style.display = "flex";
+            document.getElementById("copy-btn").style.display = "flex";
         }
         else {
             document.meta_form.result.value = `<meta name="title" content="${name}">` + "\n" + `<meta name="description" content="${disc}">` + "\n" + `<meta name="keywords" content="${key}">` + "\n" + `<meta name="author" content="${auth}">` + "\n" + `<meta property="og:url" content="${url}">` + "\n" + `<meta property="og:title" content="${name}">` + "\n" + `<meta property="og:description" content="${disc}">` + "\n" + `<meta property="og:image" itemprop="image" content="${img}">` + "\n" + `<meta http-equiv="refresh" content="${refresh}">` + "\n" + `<meta name="revisit-after" content="${revisit}days">` + "\n" + `<meta property="og:type" content="website">` + "\n" + `<meta name="robots" content="index,follow">`;
+            document.meta_form.copybtn.value = "Copy 📋";
+            document.getElementById("copy-btn").style.backgroundColor = "rgb(1, 64, 254)";
+            document.getElementById("resultinp").style.display = "flex";
+            document.getElementById("copy-btn").style.display = "flex";
         }
     }
 }
