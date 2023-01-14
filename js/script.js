c6b5223c13989972c6f4426606d80f0c81a539e7
Harsh <harshj7.net@gmail.com>
2023-01-14 11:31:14 +0530

    working if condition expect author name

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function metadata() {
-    if (meta_form.meta_name.value == "" && meta_form.meta_disc.value == "" && meta_form.meta_key.value == "" && meta_form.meta_author.value == "") {
+    if (meta_form.meta_name.value == "" || meta_form.meta_disc.value == "" || meta_form.meta_key.value == "" || meta_form.meta_url.value == "" || meta_form.metaimg_url.value == "") {
         alert("Sorry, Please enter required value");
     }
     else {
@@ -11,6 +11,22 @@ function metadata() {
         let img = document.meta_form.metaimg_url.value;
         let refresh = document.meta_form.meta_refreshsec.value;
         let revisit = document.meta_form.meta_enginevisit.value;
-        document.meta_form.result.value = `<meta name="title" content="${name}">` + "\n" + `<meta name="description" content="${disc}">` + "\n" + `<meta name="keywords" content="${key}">` + "\n" + `<meta name="author" content="${auth}">` + "\n" + `<meta property="og:url" content="${url}">` + "\n" + `<meta property="og:title" content="${name}">` + "\n" + `<meta property="og:description" content="${disc}">` + "\n" + `<meta property="og:image" itemprop="image" content="${img}">` + "\n" + `<meta http-equiv="refresh" content="${refresh}">` + "\n" + `<meta name="revisit-after" content="${revisit}days">` + "\n" + `<meta property="og:type" content="website">` + "\n" + `<meta name="robots" content="index,follow">`;
+
+        if (meta_form.meta_refreshsec.value == "" && meta_form.meta_enginevisit.value == "") {
+            refresh = 3000000;
+            revisit = 5;
+            document.meta_form.result.value = `<meta name="title" content="${name}">` + "\n" + `<meta name="description" content="${disc}">` + "\n" + `<meta name="keywords" content="${key}">` + "\n" + `<meta name="author" content="${auth}">` + "\n" + `<meta property="og:url" content="${url}">` + "\n" + `<meta property="og:title" content="${name}">` + "\n" + `<meta property="og:description" content="${disc}">` + "\n" + `<meta property="og:image" itemprop="image" content="${img}">` + "\n" + `<meta http-equiv="refresh" content="${refresh}">` + "\n" + `<meta name="revisit-after" content="${revisit}days">` + "\n" + `<meta property="og:type" content="website">` + "\n" + `<meta name="robots" content="index,follow">`;
+        }
+        else if (meta_form.meta_refreshsec.value == "") {
+            refresh = 3000000;
+            document.meta_form.result.value = `<meta name="title" content="${name}">` + "\n" + `<meta name="description" content="${disc}">` + "\n" + `<meta name="keywords" content="${key}">` + "\n" + `<meta name="author" content="${auth}">` + "\n" + `<meta property="og:url" content="${url}">` + "\n" + `<meta property="og:title" content="${name}">` + "\n" + `<meta property="og:description" content="${disc}">` + "\n" + `<meta property="og:image" itemprop="image" content="${img}">` + "\n" + `<meta http-equiv="refresh" content="${refresh}">` + "\n" + `<meta name="revisit-after" content="${revisit}days">` + "\n" + `<meta property="og:type" content="website">` + "\n" + `<meta name="robots" content="index,follow">`;
+        }
+        else if (meta_form.meta_enginevisit.value == "") {
+            revisit = 5;
+            document.meta_form.result.value = `<meta name="title" content="${name}">` + "\n" + `<meta name="description" content="${disc}">` + "\n" + `<meta name="keywords" content="${key}">` + "\n" + `<meta name="author" content="${auth}">` + "\n" + `<meta property="og:url" content="${url}">` + "\n" + `<meta property="og:title" content="${name}">` + "\n" + `<meta property="og:description" content="${disc}">` + "\n" + `<meta property="og:image" itemprop="image" content="${img}">` + "\n" + `<meta http-equiv="refresh" content="${refresh}">` + "\n" + `<meta name="revisit-after" content="${revisit}days">` + "\n" + `<meta property="og:type" content="website">` + "\n" + `<meta name="robots" content="index,follow">`;
+        }
+        else {
+            document.meta_form.result.value = `<meta name="title" content="${name}">` + "\n" + `<meta name="description" content="${disc}">` + "\n" + `<meta name="keywords" content="${key}">` + "\n" + `<meta name="author" content="${auth}">` + "\n" + `<meta property="og:url" content="${url}">` + "\n" + `<meta property="og:title" content="${name}">` + "\n" + `<meta property="og:description" content="${disc}">` + "\n" + `<meta property="og:image" itemprop="image" content="${img}">` + "\n" + `<meta http-equiv="refresh" content="${refresh}">` + "\n" + `<meta name="revisit-after" content="${revisit}days">` + "\n" + `<meta property="og:type" content="website">` + "\n" + `<meta name="robots" content="index,follow">`;
+        }
     }
 }
